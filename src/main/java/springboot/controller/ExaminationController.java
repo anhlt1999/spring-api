919419f2e343cdf57f19fd32cac8b49d956efccc
anhlt1999 <tal180899@gmail.com>
2020-12-17 13:18:50 +0700

    aa

--- src/main/java/springboot/controller/ExaminationController.java
+++ src/main/java/springboot/controller/ExaminationController.java
@@ -20,7 +20,7 @@ import springboot.exception.ResourceNotFoundException;
 import springboot.model.Examination;
 import springboot.repository.ExaminationRespository;
 
-@CrossOrigin(origins = "https://ltwebptit.herokuapp.com")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class ExaminationController {
--- src/main/java/springboot/model/Examination.java
+++ src/main/java/springboot/model/Examination.java
@@ -10,46 +10,41 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "Examination")
 public class Examination {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long examinationID;
 
-	@Column(name = "patientID")
-	private long patientID;
-
 	@Column(name = "doctorID")
-	private long doctorID;
+	private int doctorID;
 
 	@Column(name = "nurseID")
-	private long nurseID;
+	private int nurseID;
 
-	@Column(name = "PrescriptionID")
-	private long PrescriptionID;
 
-	@Column(name = "in")
+	@Column(name = "patientID")
+	private int patientID;
+	@Column(name = "prescriptionID")
+	private int prescriptionID;
+	@Column(name = "indate")
 	private String in;
-
-	@Column(name = "out")
+	@Column(name = "outdate")
 	private String out;
-
 	@Column(name = "disease")
 	private String disease;
-
 	@Column(name = "totalMoney")
-	private float totalMoney;
-
+	private String totalMoney;
 	public Examination() {
-		super();
+
 	}
 
-	public Examination(long examinationID, long patientID, long doctorID, long nurseID, long prescriptionID, String in,
-			String out, String disease, float totalMoney) {
+	public Examination(long examinationID, int doctorID, int nurseID, int patientID, int prescriptionID, String in, String out, String disease, String totalMoney) {
 		super();
 		this.examinationID = examinationID;
-		this.patientID = patientID;
 		this.doctorID = doctorID;
 		this.nurseID = nurseID;
-		this.PrescriptionID = prescriptionID;
+		this.patientID = patientID;
+		this.prescriptionID = prescriptionID;
 		this.in = in;
 		this.out = out;
 		this.disease = disease;
@@ -64,36 +59,36 @@ public class Examination {
 		this.examinationID = examinationID;
 	}
 
-	public long getPatientID() {
-		return patientID;
-	}
-
-	public void setPatientID(long patientID) {
-		this.patientID = patientID;
-	}
-
-	public long getDoctorID() {
+	public int getDoctorID() {
 		return doctorID;
 	}
 
-	public void setDoctorID(long doctorID) {
+	public void setDoctorID(int doctorID) {
 		this.doctorID = doctorID;
 	}
 
-	public long getNurseID() {
+	public int getNurseID() {
 		return nurseID;
 	}
 
-	public void setNurseID(long nurseID) {
+	public void setNurseID(int nurseID) {
 		this.nurseID = nurseID;
 	}
 
-	public long getPrescriptionID() {
-		return PrescriptionID;
+	public int getPatientID() {
+		return patientID;
 	}
 
-	public void setPrescriptionID(long prescriptionID) {
-		PrescriptionID = prescriptionID;
+	public void setPatientID(int patientID) {
+		this.patientID = patientID;
+	}
+
+	public int getPrescriptionID() {
+		return prescriptionID;
+	}
+
+	public void setPrescriptionID(int prescriptionID) {
+		this.prescriptionID = prescriptionID;
 	}
 
 	public String getIn() {
@@ -120,14 +115,11 @@ public class Examination {
 		this.disease = disease;
 	}
 
-	public float getTotalMoney() {
+	public String getTotalMoney() {
 		return totalMoney;
 	}
 
-	public void setTotalMoney(float totalMoney) {
+	public void setTotalMoney(String totalMoney) {
 		this.totalMoney = totalMoney;
 	}
-	
-	
-
 }
