c132a5cd5c886187a99312cc48e8044b97d73677
anhlt1999 <tal180899@gmail.com>
2020-12-17 13:30:28 +0700

    Update ExaminationController.java

--- src/main/java/springboot/controller/ExaminationController.java
+++ src/main/java/springboot/controller/ExaminationController.java
@@ -20,7 +20,7 @@ import springboot.exception.ResourceNotFoundException;
 import springboot.model.Examination;
 import springboot.repository.ExaminationRespository;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://ltwebptit.herokuapp.com")
 @RestController
 @RequestMapping("/api/v1/")
 public class ExaminationController {
